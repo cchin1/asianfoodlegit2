262a4174221b7b38c35213d1e1e73f8861368d82
Candice Chin <chincandice2007@gmail.com>
2020-01-15 20:56:00 -0800

    stopping point on public js files in backend

---
+++ backend/public/js/add.js
@@ -0,0 +1,23 @@
+$("#add-btn").on("click", function(event){
+    event.preventDefault();
+
+    var newReview = {
+        author: $("#author").val().trim(),
+        restaurant: $("#restaurant").val().trim(),
+        cuisine: $("#cuisine").val().trim(),
+        country: $("#cuisine").val().trim(),
+        text: $("#text").val().trim(),
+    }
+
+    $.post("/api/new", newReview)
+    .done(function(data){
+        console.log(data);
+    });
+
+    $("#author").val("");
+    $("#restaurant").val("");
+    $("#cuisine").val("");
+    $("#country").val("");
+    $("#text").val("");
+
+});
---
+++ backend/public/js/all.js
@@ -0,0 +1,8 @@
+$.get("/api/all", function(data){
+    console.log(data);
+    for (var i=0; i<data.length; i++) {
+        var wellSection = $("<div>");
+    }
+})
+
+// This is unfinished
