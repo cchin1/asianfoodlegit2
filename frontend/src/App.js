1991f658f3b398dc62b57610033995eebdaf5cc4
Ohmydearcheesus <ohmydearcheesus@gmail.com>
2020-01-16 14:19:29 -0800

    Add bulma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+
 import logo from "./logo.svg";
 import "./App.css";
 import constants from "./constants";
 import "./style.css";
 import "./index.css";
-import "react-bulma-components/dist/react-bulma-components.min.css";
-import { Button } from "react-bulma-components/lib/components/button";
-import "./img/logo.png";
+import { Button } from "react-bulma-components";
+import logoImage from "./img/logo.png";
 
 // Rendering and calling the API from React to the Express Server
 
@@ -17,18 +18,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav class="navbar is-transparent">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="index.html">
+        <nav className="navbar is-transparent">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="index.html">
               <img
-                src="./img/logo.png"
+                src={logoImage}
                 alt="Bulma: a modern CSS framework based on Flexbox"
                 width="112"
                 height="28"
               />
             </a>
             <div
-              class="navbar-burger burger"
+              className="navbar-burger burger"
               data-target="navbarExampleTransparentExample"
             >
               <span></span>
@@ -37,35 +38,35 @@ class App extends Component {
             </div>
           </div>
 
-          <div id="navbarExampleTransparentExample" class="navbar-menu ">
-            <div class="navbar-start">
-              <a class="navbar-item" href="#">
+          <div id="navbarExampleTransparentExample" className="navbar-menu ">
+            <div className="navbar-start">
+              <a className="navbar-item" href="#">
                 Sushi
               </a>
 
-              <a class="navbar-item" href="#">
+              <a className="navbar-item" href="#">
                 Dumplings
               </a>
 
-              <a class="navbar-item" href="#">
+              <a className="navbar-item" href="#">
                 Noodles
               </a>
 
-              <a class="navbar-item" href="#">
+              <a className="navbar-item" href="#">
                 Hot Pot
               </a>
             </div>
           </div>
 
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="field is-grouped">
-                <Button id="loginBTN" class="Button is-danger is-outlined">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <Button id="loginBTN" className="Button is-danger is-outlined">
                   Log In
                 </Button>
 
-                <p class="control">
-                  <a href="signup.html" class="Button is-danger">
+                <p className="control">
+                  <a href="signup.html" className="Button is-danger">
                     <span>Sign Up</span>
                   </a>
                 </p>
@@ -75,11 +76,11 @@ class App extends Component {
         </nav>
 
         <section id="page_header">
-          <div class="page_title">
-            <div class="container">
-              <div class="row">
-                <div class="col-md-12">
-                  <h2 class="title upper">Our Legit</h2>
+          <div className="page_title">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <h2 className="title upper">Our Legit</h2>
                   <p>The Most Authentic Asian Places</p>
                 </div>
               </div>
@@ -88,17 +89,17 @@ class App extends Component {
         </section>
 
         <section id="Card">
-          <div class="Columns">
-            <div class="Column is-two-fifths">
-              <figure class="image is-3by2">
+          <div className="Columns">
+            <div className="Column is-two-fifths">
+              <figure className="image is-3by2">
                 <img src="https://bulma.io/images/placeholders/256x256.png" />
               </figure>
               <br />
             </div>
           </div>
-          <div class="Column">
-            <h1 class="title is-3">Name:</h1>
-            <div class="rating">
+          <div className="Column">
+            <h1 className="title is-3">Name:</h1>
+            <div className="rating">
               <input name="stars" id="e5" type="radio" />
               <label for="e5">☆</label>
               <input name="stars" id="e4" type="radio" />
@@ -111,7 +112,7 @@ class App extends Component {
               <label for="e1">☆</label>
             </div>
 
-            <div class="review">
+            <div className="review">
               <p>
                 At vero eos et accusamus et iusto odio dignissimos ducimus qui
                 blanditiis praesentium voluptatum deleniti atque corrupti quos
@@ -129,106 +130,106 @@ class App extends Component {
 
         <section id="review">
           <Button
-            class="Button is-danger is-outlined"
+            className="Button is-danger is-outlined"
             onclick="window.location.href = 'login.html';"
           >
             Add Your Review
           </Button>
         </section>
-        <div class="modal" id="login">
-          <div class="modal-background"></div>
-          <div class="modal-Card">
-            <form action="" class="box">
-              <div class="field">
-                <label for="" class="label">
+        <div className="modal" id="login">
+          <div className="modal-background"></div>
+          <div className="modal-Card">
+            <form action="" className="box">
+              <div className="field">
+                <label for="" className="label">
                   Email
                 </label>
-                <div class="control has-icons-left">
+                <div className="control has-icons-left">
                   <input
                     type="email"
                     placeholder="e.g. bobsmith@gmail.com"
-                    class="input"
+                    className="input"
                     required
                   ></input>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
+                  <span className="icon is-small is-left">
+                    <i className="fa fa-envelope"></i>
                   </span>
                 </div>
               </div>
-              <div class="field">
-                <label for="" class="label">
+              <div className="field">
+                <label for="" className="label">
                   Password
                 </label>
-                <div class="control has-icons-left">
+                <div className="control has-icons-left">
                   <input
                     type="password"
                     placeholder="*******"
-                    class="input"
+                    className="input"
                     required
                   ></input>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
+                  <span className="icon is-small is-left">
+                    <i className="fa fa-lock"></i>
                   </span>
                 </div>
               </div>
-              <div class="field">
-                <label for="" class="checkbox">
+              <div className="field">
+                <label for="" className="checkbox">
                   <input type="checkbox" />
                   Remember me
                 </label>
               </div>
-              <div class="field">
-                <Button class="Button is-success">Login</Button>
+              <div className="field">
+                <Button className="Button is-success">Login</Button>
               </div>
               <a href="signup.html"> Don't you have an account? Sign up now!</a>
             </form>
           </div>
         </div>
 
-        <div class="modal" id="signup">
-          <div class="modal-background"></div>
-          <div class="modal-Card">
-            <form action="" class="box" />
-            <div class="login Column is-6">
-              <section class="section">
-                <div class="has-text-centered">
-                  <img class="login-logo" src="img/logo.png" />
+        <div className="modal" id="signup">
+          <div className="modal-background"></div>
+          <div className="modal-Card">
+            <form action="" className="box" />
+            <div className="login Column is-6">
+              <section className="section">
+                <div className="has-text-centered">
+                  <img className="login-logo" src="img/logo.png" />
                 </div>
 
-                <div class="field">
-                  <label class="label">Username</label>
-                  <div class="control has-icons-right">
-                    <input class="input" type="text" />
-                    <span class="icon is-small is-right">
-                      <i class="fa fa-user"></i>
+                <div className="field">
+                  <label className="label">Username</label>
+                  <div className="control has-icons-right">
+                    <input className="input" type="text" />
+                    <span className="icon is-small is-right">
+                      <i className="fa fa-user"></i>
                     </span>
                   </div>
                 </div>
 
-                <div class="field">
-                  <label class="label">Email</label>
-                  <div class="control has-icons-right">
-                    <input class="input" type="text" />
-                    <span class="icon is-small is-right">
-                      <i class="fa fa-envelope"></i>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control has-icons-right">
+                    <input className="input" type="text" />
+                    <span className="icon is-small is-right">
+                      <i className="fa fa-envelope"></i>
                     </span>
                   </div>
                 </div>
 
-                <div class="field">
-                  <label class="label">Password</label>
-                  <div class="control has-icons-right">
-                    <input class="input" type="password" />
-                    <span class="icon is-small is-right">
-                      <i class="fa fa-key"></i>
+                <div className="field">
+                  <label className="label">Password</label>
+                  <div className="control has-icons-right">
+                    <input className="input" type="password" />
+                    <span className="icon is-small is-right">
+                      <i className="fa fa-key"></i>
                     </span>
                   </div>
                 </div>
-                <div class="has-text-centered">
-                  <a class="Button is-vcentered is-primary">Sign Up!</a>
+                <div className="has-text-centered">
+                  <a className="Button is-vcentered is-primary">Sign Up!</a>
                 </div>
                 <br />
-                <div class="has-text-centered">
+                <div className="has-text-centered">
                   <a href="login.html">
                     {" "}
                     Already have an account? Log in now !
@@ -236,103 +237,103 @@ class App extends Component {
                 </div>
               </section>
             </div>
-            <div id="particles-js" class="interactive-bg Column is-8"></div>
+            <div id="particles-js" className="interactive-bg Column is-8"></div>
           </div>
         </div>
 
-        <div class="modal" id="login">
-          <div class="modal-background"></div>
-          <div class="modal-Card">
-            <form action="" class="box">
-              <div class="field">
-                <label for="" class="label">
+        <div className="modal" id="login">
+          <div className="modal-background"></div>
+          <div className="modal-Card">
+            <form action="" className="box">
+              <div className="field">
+                <label for="" className="label">
                   Email
                 </label>
-                <div class="control has-icons-left">
+                <div className="control has-icons-left">
                   <input
                     type="email"
                     placeholder="e.g. bobsmith@gmail.com"
-                    class="input"
+                    className="input"
                     required
                   />
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
+                  <span className="icon is-small is-left">
+                    <i className="fa fa-envelope"></i>
                   </span>
                 </div>
               </div>
-              <div class="field">
-                <label for="" class="label">
+              <div className="field">
+                <label for="" className="label">
                   Password
                 </label>
-                <div class="control has-icons-left">
+                <div className="control has-icons-left">
                   <input
                     type="password"
                     placeholder="*******"
-                    class="input"
+                    className="input"
                     required
                   />
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
+                  <span className="icon is-small is-left">
+                    <i className="fa fa-lock"></i>
                   </span>
                 </div>
               </div>
-              <div class="field">
-                <label for="" class="checkbox">
+              <div className="field">
+                <label for="" className="checkbox">
                   <input type="checkbox" />
                   Remember me
                 </label>
               </div>
-              <div class="field">
-                <Button class="Button is-success">Login</Button>
+              <div className="field">
+                <Button className="Button is-success">Login</Button>
               </div>
               <a href="signup.html"> Don't you have an account? Sign up now!</a>
             </form>
           </div>
         </div>
 
-        <section class="modal-Card-body">
-          <div class="modal-content">
-            <form action="" class="box">
-              <div class="field">
-                <label for="" class="label">
+        <section className="modal-Card-body">
+          <div className="modal-content">
+            <form action="" className="box">
+              <div className="field">
+                <label for="" className="label">
                   Email
                 </label>
-                <div class="control has-icons-left">
+                <div className="control has-icons-left">
                   <input
                     type="email"
                     placeholder="e.g. bobsmith@gmail.com"
-                    class="input"
+                    className="input"
                     required
                   />
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
+                  <span className="icon is-small is-left">
+                    <i className="fa fa-envelope"></i>
                   </span>
                 </div>
               </div>
-              <div class="field">
-                <label for="" class="label">
+              <div className="field">
+                <label for="" className="label">
                   Password
                 </label>
-                <div class="control has-icons-left">
+                <div className="control has-icons-left">
                   <input
                     type="password"
                     placeholder="*******"
-                    class="input"
+                    className="input"
                     required
                   />
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
+                  <span className="icon is-small is-left">
+                    <i className="fa fa-lock"></i>
                   </span>
                 </div>
               </div>
-              <div class="field">
-                <label for="" class="checkbox">
+              <div className="field">
+                <label for="" className="checkbox">
                   <input type="checkbox" />
                   Remember me
                 </label>
               </div>
-              <div class="field">
-                <Button class="Button is-success">Login</Button>
+              <div className="field">
+                <Button className="Button is-success">Login</Button>
               </div>
               <a href="signup.html"> Don't you have an account? Sign up now!</a>
             </form>
