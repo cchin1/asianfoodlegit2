14fdee423f33516242610418e7ca5e084086087b
Candice Chin <chincandice2007@gmail.com>
2020-01-15 18:49:21 -0800

    added password for MySQL

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -4,7 +4,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sequelize_asianfoodlegit2", "root", "", {
+var sequelize = new Sequelize("sequelize_asianfoodlegit2", "root", "JS123$$$", {
     host: "localhost",
     port: 3306,
     dialect: "mysql",
