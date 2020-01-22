4f1387958f4cc1d07f27a539aa212ce9cc5b1cda
Ohmydearcheesus <ohmydearcheesus@gmail.com>
2020-01-22 09:01:53 -0800

    fixed rating, now it updates to the server as well

--- frontend/src/components/Add/Rating.js
+++ frontend/src/components/Add/Rating.js
@@ -6,10 +6,15 @@ import "../../index.css";
 
 class NewRating extends Component {
   render() {
+    const { updateRating } = this.props;
     return (
       <div>
         Restaurant Rating:
-        <input placeholder="1 - 5" style={{ width: "90%" }}></input>
+        <input
+          placeholder="1 - 5"
+          style={{ width: "90%" }}
+          onChange={event => updateRating(event.target.value)}
+        ></input>
         <br />
         <br />
       </div>
